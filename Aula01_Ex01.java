5a3dffbe87270e61c57bf02118299845606170d3
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-08-07 22:08:31 -0300

    Create Aula01_Ex01.java

---
+++ Aula01_Ex01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Informe a quantidade de livros adquiridos: ");
+    Scanner leitor = new Scanner (System.in);
+    
+    //carrega a quantidade de livros adquiridos na variável qtddLivros
+    double qtddLivros = leitor.nextDouble();
+    
+    if (qtddLivros<0){
+      System.out.println("Erro");
+    } else {
+      double criterioA = (qtddLivros*0.25) + 7.5;
+      double criterioB = (qtddLivros*0.5) + 2.5;
+
+      if (criterioA>criterioB){
+        System.out.println("Criterio B");
+      } else if (criterioB>criterioA){
+        System.out.println("Criterio A");
+      } else {
+        System.out.println("Indiferente");
+      }
+    }
+  }
+}
