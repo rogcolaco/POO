dcae265c3cc9eb6f90456a5f1d9f42ad7527c576
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-08-16 18:38:49 -0300

    Update Aula02_Ex02.java

--- Aula02_Ex02.java
+++ Aula02_Ex02.java
@@ -9,18 +9,13 @@ public class Main {
         C = new int [5];
 
         Scanner leitor = new Scanner (System.in);
-        A[0] = leitor.nextInt();
-        A[1] = leitor.nextInt();
-        A[2] = leitor.nextInt();
-        A[3] = leitor.nextInt();
-        A[4] = leitor.nextInt();
+        for (i=0;i<5;i++){
+            A[i]=leitor.nextInt();
+        }
 
-        Scanner leitorB = new Scanner (System.in);
-        B[0] = leitorB.nextInt();
-        B[1] = leitorB.nextInt();
-        B[2] = leitorB.nextInt();
-        B[3] = leitorB.nextInt();
-        B[4] = leitorB.nextInt();
+        for (i=0;i<5;i++){
+            B[i]=leitor.nextInt();
+        }
 
         for (i=0; i<5; i++){
             C[i]=A[i]+B[i];
