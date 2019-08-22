a45bb7b48ae371680a4080567aeb406dfbbd903f
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-08-22 14:11:25 -0300

    Update Departament.java

--- Aula03/Departament.java
+++ Aula03/Departament.java
@@ -1,2 +1,58 @@
 public class Departament {
+
+    //DECLARAÇÃO ATRIBUTOS DA CLASSE
+    String name, location;
+    int code, phoneExtention;
+    double budget;
+
+    //CONSTRUCTORS
+    public Departament(int code) {
+        this.code = code;
+    }
+
+    public Departament(String name, int code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    //SETS AND GETS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getPhoneExtention() {
+        return phoneExtention;
+    }
+
+    public void setPhoneExtention(int phoneExtention) {
+        this.phoneExtention = phoneExtention;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
 }
