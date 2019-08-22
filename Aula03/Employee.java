7b5096d45d1979a87d318b8bcb6d05f0a489526b
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-08-22 14:45:17 -0300

    Update Employee.java

--- Aula03/Employee.java
+++ Aula03/Employee.java
@@ -14,9 +14,15 @@ public class Employee {
         this.name = name;
         this.id = id;
     }
+    
+    public Employee(String name, String jobTitle, long id, double salary) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.id = id;
+        this.salary = salary;
+    }
 
     //SETS AND GETS
-
     public String getName() {
         return name;
     }
@@ -48,4 +54,14 @@ public class Employee {
     public void setSalary(double salary) {
         this.salary = salary;
     }
+
+    //METHODS
+    public void printState(){
+        System.out.println(this.id+"\t"+this.name+"\t"+this.jobTitle+"\t"+this.salary);
+    }
+
+    public void getAnnualSalary(){
+        System.out.println(this.salary*12);
+    }
+
 }
