31a76cc43559755db3a411ef9a25e5a7b694c333
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-08-22 14:45:37 -0300

    Update Principal.java

--- Aula03/Principal.java
+++ Aula03/Principal.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
 
+        int i,d;
+        String nome;
+
+        Scanner leitor = new Scanner(System.in);
+
+        /*for(i=0;i<3;i++){
+            d=leitor.nextInt();
+            Employee empregado = new Employee(d);
+        }*/
+
+        Employee empregado = new Employee(123);
+
+        empregado.printState();
+        empregado.setName("Rogerio");
+        empregado.setSalary(1200.10);
+        empregado.printState();
+        empregado.getAnnualSalary();
     }
 }
