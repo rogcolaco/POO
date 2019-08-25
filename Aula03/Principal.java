a301196366c336b7da5ec95b2d6c4e703a73dde4
rogcolaco <50340398+rogcolaco@users.noreply.github.com>
2019-08-25 20:06:09 -0300

    Update Principal.java

--- Aula03/Principal.java
+++ Aula03/Principal.java
@@ -17,18 +17,25 @@ public class Principal {
 
         Departament departament1 = new Departament("WEB",001);
 
+        //EMPREGADO SEM DADOS PARA INICIALIZAR O ARRAY
+        Employee employeeSet = new Employee ("set","set",000,000);
+
 
         //PROBLEMA PARA SER RESOLVIDO COMO O OBJETO PODE SER CRIADO COM ASSOCIAÇÃO COM DEPARTAMENT?
-        Employee employee1 = new Employee(101);
-        employee1.setName("Rogerio");
+        Employee employee1 = new Employee("Rogério","Desocupado",101,1.000);
         employee1.setDepartament(departament1);
         Employee employee3 = new Employee(103);
         employee3.setDepartament(departament1);
+        employee3.setName("Monica");
 
         employee1.printState();
         employee3.printState();
 
         System.out.println("\n");
+        for(i=0;i<departamentList1.length;i++){
+            employeeSet = new Employee("set", "set", 000, 000);
+            departamentList1[i]=employeeSet;
+        }
 
         departamentList1[0]=employee1;
 
@@ -37,7 +44,9 @@ public class Principal {
         System.out.println("\n");
         departament1.printState();
         d=departament1.sizeOfEmployees();
-        System.out.println(d);
-
+        System.out.printf("A quantidade de funcionarios no departamento 1 é: %d \n", d);
+        System.out.println("\n");
+        departament1.addEmployee(employee3);
+        departament1.printState();
     }
 }
